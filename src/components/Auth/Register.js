edd7c5ddaca6ea44b733f2e2338c9692b642dff8
Tyler Leung <binglux@gmail.com>
2019-09-18 22:53:07 +0800

    feat(form-validation): validation check of form data

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -12,10 +12,12 @@ import {
 import firebase from '../../firebase';
 
 const Register = () => {
-  const [username, setUsername] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
-  const [passwordConfirmation, setPasswordConfirmation] = useState(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = event => {
     const value = event.target.value;
@@ -38,17 +40,62 @@ const Register = () => {
     }
   };
 
+  const isFormValid = () => {
+    let error;
+
+    if (isFormEmpty()) {
+      error = { message: 'Fill in all fields' };
+      setErrors([error, ...errors]);
+    } else if (!isPasswordValid()) {
+      error = { message: 'Password is invalid.' }
+      setErrors([error, ...errors]);
+    } else {
+      return true;
+    }
+  };
+
+  const isFormEmpty = () => {
+    return !username.length || !email.length ||
+      !password.length || !passwordConfirmation.length;
+  };
+
+  const isPasswordValid = () => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else {
+      return password === passwordConfirmation;
+    }
+  };
+
+  const displayErrors = () => errors.map((error, i) => {
+    return <p key={i}>{ error.message }</p>;
+  });
+
   const handleSubmit = event => {
     event.preventDefault();
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(createdUser => {
-        console.log(createdUser);
-      })
-      .catch(error => {
-        console.error(error);
-      })
+    if (isFormValid()) {
+      setLoading(true);
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password)
+        .then(createdUser => {
+          console.log(createdUser);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.error(error);
+          setLoading(false);
+          setErrors([error, ...errors])
+        })
+    }
+  };
+
+  const handleInputError = inputName => {
+    return errors.some(error =>
+      error.message.toLowerCase().includes(inputName)
+    )
+      ? "error"
+      : ""
   };
 
   return (
@@ -61,48 +108,70 @@ const Register = () => {
         <Form onSubmit={handleSubmit} size="large">
           <Segment stacked>
             <Form.Input
-              fluid name="username"
+              fluid
+              name="username"
               icon="user"
               iconPosition="left"
               placeholder="Username"
               onChange={handleChange}
               type="text"
               value={username}
+              className={handleInputError("username")}
             />
 
             <Form.Input
-              fluid name="email"
+              fluid
+              name="email"
               icon="mail"
               iconPosition="left"
               placeholder="Email Address"
               onChange={handleChange}
               type="text"
               value={email}
+              className={handleInputError("email")}
             />
 
             <Form.Input
-              fluid name="password"
+              fluid
+              name="password"
               icon="lock"
               iconPosition="left"
               placeholder="Password"
               onChange={handleChange}
               type="password"
               value={password}
+              className={handleInputError("password")}
             />
 
             <Form.Input
-              fluid name="passwordConfirmation"
+              fluid
+              name="passwordConfirmation"
               icon="lock"
               iconPosition="left"
               placeholder="Password Conformation"
               onChange={handleChange}
               type="password"
               value={passwordConfirmation}
+              className={handleInputError("password")}
             />
 
-            <Button color="orange" fluid size="large">Submit</Button>
+            <Button
+              disabled={loading}
+              className={ loading ? 'loading' : '' }
+              color="orange"
+              fluid size="large"
+            >
+              Submit
+            </Button>
+
           </Segment>
         </Form>
+        { errors.length > 0 && (
+          <Message error>
+            <h3>Error</h3>
+            { displayErrors() }
+          </Message>
+        )}
         <Message>Already a user? <Link to="/login">Login</Link></Message>
       </Grid.Column>
     </Grid>
